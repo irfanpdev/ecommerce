55d0b8ec746020aea4db2e561aa65ad2250f174b
irfanpdev <144534138+irfanpdev@users.noreply.github.com>
2023-09-20 19:34:16 +0530

    Update utils.py

--- ecomaut/utils.py
+++ ecomaut/utils.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 def send_email_token(email,token):
     try:
         subject = 'Your account need to be verified'
-        message = f'Click on the link to verify http://127.0.0.1:8000/ecomaut/account-verify/{token}'
+        message = f'Click on the link to verify https://ecommercesrv.onrender.com/ecomaut/account-verify/{token}'
         email_from =settings.EMAIL_HOST_USER
         recipent_list= [email]
         send_mail(subject,message,email_from,recipent_list)
@@ -15,7 +15,7 @@ def send_email_token(email,token):
 def send_email_reset_token(email,forgetPasstoken):
     try:
         subject = 'Reset Password'
-        message = f'Click on the link to reset password http://127.0.0.1:8000/ecomaut/forgetpass_link_activate/{forgetPasstoken}'
+        message = f'Click on the link to reset password https://ecommercesrv.onrender.com/ecomaut/forgetpass_link_activate/{forgetPasstoken}'
         email_from =settings.EMAIL_HOST_USER
         recipent_list= [email]
         send_mail(subject,message,email_from,recipent_list)
